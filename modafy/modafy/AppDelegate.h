f5e25a162595ecca5e54347a19f45f6c73d5a016
Trevor Dowds <tdowds@berkeley.edu>
2014-07-11 19:05:49 +0200

    Loading project

---
+++ modafy/modafy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  modafy
+//
+//  Created by Trevor Dowds on 7/11/14.
+//  Copyright (c) 2014 Trevor Dowds and Maura Fitzgerald. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ modafy/modafyTests/modafyTests.m
@@ -0,0 +1,34 @@
+//
+//  modafyTests.m
+//  modafyTests
+//
+//  Created by Trevor Dowds on 7/11/14.
+//  Copyright (c) 2014 Trevor Dowds and Maura Fitzgerald. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface modafyTests : XCTestCase
+
+@end
+
+@implementation modafyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
