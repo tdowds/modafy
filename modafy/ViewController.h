f24ebbbc4235dc5c9e020d0bd55d79791eec7ac8
Trevor Dowds <tdowds@berkeley.edu>
2014-07-14 21:51:25 +0200

    Added ViewController

---
+++ modafy/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Modafy
+//
+//  Created by Trevor Dowds on 7/14/14.
+//  Copyright (c) 2014 Modafy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
