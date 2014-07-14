86b92fb587910d4c1f78e981e7c4c4549d84ef2b
Trevor Dowds <tdowds@berkeley.edu>
2014-07-14 17:55:55 +0200

    back to ios

---
+++ modafy/ModafyTests/ModafyTests.m
@@ -0,0 +1,34 @@
+//
+//  ModafyTests.m
+//  ModafyTests
+//
+//  Created by Trevor Dowds on 7/14/14.
+//  Copyright (c) 2014 Modafy. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ModafyTests : XCTestCase
+
+@end
+
+@implementation ModafyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
