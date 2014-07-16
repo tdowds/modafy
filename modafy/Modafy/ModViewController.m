9fbb41681833ca1a477501005eb83e559789dbf3
Trevor Dowds <tdowds@berkeley.edu>
2014-07-16 13:30:35 +0200

    added more mock cloth pngs

--- modafy/Modafy/ModViewController.m
+++ modafy/Modafy/ModViewController.m
@@ -72,20 +72,38 @@
     ModShirt *firstShirt = [[ModShirt alloc] initWithColor:(@"coral") size:(@"medium") type:(@"shirt") image:([UIImage imageNamed:(@"t12")])];
     ModShirt *secondShirt = [[ModShirt alloc] initWithColor:(@"red") size:(@"small") type:(@"shirt") image:([UIImage imageNamed:(@"t5")])];
     ModShirt *thirdShirt = [[ModShirt alloc] initWithColor:(@"black") size:(@"x-small") type:(@"shirt") image:([UIImage imageNamed:(@"t2")])];
+    ModShirt *fourthShirt = [[ModShirt alloc] initWithColor:(@"black") size:(@"x-small") type:(@"shirt") image:([UIImage imageNamed:(@"t1")])];
+    ModShirt *fifthShirt = [[ModShirt alloc] initWithColor:(@"black") size:(@"x-small") type:(@"shirt") image:([UIImage imageNamed:(@"t9")])];
+    ModShirt *sixthShirt = [[ModShirt alloc] initWithColor:(@"black") size:(@"x-small") type:(@"shirt") image:([UIImage imageNamed:(@"t10")])];
+    ModShirt *seventhShirt = [[ModShirt alloc] initWithColor:(@"black") size:(@"x-small") type:(@"shirt") image:([UIImage imageNamed:(@"t14")])];
     
     //PANTS
     ModPants *firstPant = [[ModPants alloc] initWithColor:@"blue" size:@"4" length:30 width:30 type:@"pants" image:[UIImage imageNamed:@"b3"]];
     ModPants *secondPant = [[ModPants alloc] initWithColor:@"salmon" size:@"4" length:30 width:30 type:@"pants" image:[UIImage imageNamed:@"b4"]];
     ModPants *thirdPant = [[ModPants alloc] initWithColor:@"striped" size:@"5" length:30 width:30 type:@"pants" image:[UIImage imageNamed:(@"b10")]];
+    ModPants *fourthPant = [[ModPants alloc] initWithColor:@"striped" size:@"5" length:30 width:30 type:@"pants" image:[UIImage imageNamed:(@"b1")]];
+    ModPants *fifthPant = [[ModPants alloc] initWithColor:@"striped" size:@"5" length:30 width:30 type:@"pants" image:[UIImage imageNamed:(@"b2")]];
+    ModPants *sixthPant = [[ModPants alloc] initWithColor:@"striped" size:@"5" length:30 width:30 type:@"pants" image:[UIImage imageNamed:(@"b5")]];
+    ModPants *seventhPant = [[ModPants alloc] initWithColor:@"striped" size:@"5" length:30 width:30 type:@"pants" image:[UIImage imageNamed:(@"b11")]];
+    ModPants *eigthPant = [[ModPants alloc] initWithColor:@"striped" size:@"5" length:30 width:30 type:@"pants" image:[UIImage imageNamed:(@"b12")]];
+    ModPants *ninthPant = [[ModPants alloc] initWithColor:@"striped" size:@"5" length:30 width:30 type:@"pants" image:[UIImage imageNamed:(@"b13")]];
+    ModPants *tenthPant = [[ModPants alloc] initWithColor:@"striped" size:@"5" length:30 width:30 type:@"pants" image:[UIImage imageNamed:(@"b14")]];
+    ModPants *eleventhPant = [[ModPants alloc] initWithColor:@"striped" size:@"5" length:30 width:30 type:@"pants" image:[UIImage imageNamed:(@"b15")]];
     
     //SHOES
     ModShoe *firstShoe = [[ModShoe alloc] initWithColor:@"" size:@"8" type:@"shoe" image:[UIImage imageNamed:@"s1"]];
     ModShoe *secondShoe = [[ModShoe alloc] initWithColor:@"" size:@"8" type:@"shoe" image:[UIImage imageNamed:@"s11"]];
     ModShoe *thirdShoe = [[ModShoe alloc] initWithColor:@"" size:@"8" type:@"shoe" image:[UIImage imageNamed:@"s15"]];
+    ModShoe *fourthShoe = [[ModShoe alloc] initWithColor:@"" size:@"8" type:@"shoe" image:[UIImage imageNamed:@"s12"]];
+    ModShoe *fifthShoe = [[ModShoe alloc] initWithColor:@"" size:@"8" type:@"shoe" image:[UIImage imageNamed:@"s20"]];
 
     //HANDBAGS
     ModBag *firstBag = [[ModBag alloc] initWithImage:[UIImage imageNamed:@"a3"]];
     ModBag *secondBag = [[ModBag alloc] initWithImage:[UIImage imageNamed:@"a4"]];
+    ModBag *thirdBag = [[ModBag alloc] initWithImage:[UIImage imageNamed:@"a1"]];
+    ModBag *fourthBag = [[ModBag alloc] initWithImage:[UIImage imageNamed:@"a2"]];
+    ModBag *fifthBag = [[ModBag alloc] initWithImage:[UIImage imageNamed:@"a5"]];
+
     
     //ACCESSORIES
     ModAccessories *firstAcc = [[ModAccessories alloc] initWithImage:[UIImage imageNamed:@"scarf"]];
@@ -97,23 +115,40 @@
     [self.store add:firstShirt];
     [self.store add:secondShirt];
     [self.store add:thirdShirt];
+    [self.store add:fourthShirt];
+    [self.store add:fifthShirt];
+    [self.store add:sixthShirt];
+    [self.store add:seventhShirt];
     [self.imageView setImage:(self.store.shirt.image)];
     
     //ADD PANTS
     [self.pantsStore add:firstPant];
     [self.pantsStore add:secondPant];
     [self.pantsStore add:thirdPant];
+    [self.pantsStore add:fourthPant];
+    [self.pantsStore add:fifthPant];
+    [self.pantsStore add:sixthPant];
+    [self.pantsStore add:seventhPant];
+    [self.pantsStore add:eigthPant];
+    [self.pantsStore add:ninthPant];
+    [self.pantsStore add:tenthPant];
+    [self.pantsStore add:eleventhPant];
     [self.bottomsView setImage:self.pantsStore.pants.image];
     
     //ADD SHOES
     [self.shoesStore add:firstShoe];
     [self.shoesStore add:secondShoe];
     [self.shoesStore add:thirdShoe];
+    [self.shoesStore add:fourthShoe];
+    [self.shoesStore add:fifthShoe];
     [self.shoesView setImage:self.shoesStore.shoe.image];
     
     //ADD BAGS
     [self.bagStore add:firstBag];
     [self.bagStore add:secondBag];
+    [self.bagStore add:thirdBag];
+    [self.bagStore add:fourthBag];
+    [self.bagStore add:fifthBag];
     [self.bagView setImage:self.bagStore.bag.image];
     
     //ADD ACCESSORIES
