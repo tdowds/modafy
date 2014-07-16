c539d789b73aae346bf890deb02350fb68623e11
momochanfitz <momochanfitz@gmail.com>
2014-07-16 17:03:38 +0200

    icons

---
+++ modafy/Modafy/ModTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ModTableViewController.h
+//  modafy
+//
+//  Created by Maura Fitzgerald on 7/16/14.
+//  Copyright (c) 2014 Modafy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModTableViewController : UITableViewController
+
+@end
